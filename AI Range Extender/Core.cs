13a6ab943dcef4002b4ae26ccf5ae64426f6916d
TheCrunchy <crunchycamsta@gmail.com>
2023-04-23 12:42:00 +0100

    Initial commit

---
+++ AI Range Extender/Core.cs
@@ -0,0 +1,61 @@
+﻿using Sandbox.Game.Entities.Cube;
+using Sandbox.Game.GameSystems;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using Torch;
+using Torch.API;
+using Torch.API.Managers;
+using Torch.API.Plugins;
+using Torch.API.Session;
+using Torch.Managers;
+using Torch.Managers.PatchManager;
+using Torch.Session;
+
+namespace AI_Range_Extender
+{
+    public class Core : TorchPluginBase
+    {
+        public static Logger Log = LogManager.GetLogger("AIRange");
+        public override void Init(ITorchBase torch)
+        {
+            base.Init(torch);
+
+            var sessionManager = Torch.Managers.GetManager<TorchSessionManager>();
+            if (sessionManager != null)
+            {
+                sessionManager.SessionStateChanged += SessionChanged;
+            }
+            SetupConfig();
+        }
+
+        private void SetupConfig()
+        {
+            FileUtils utils = new FileUtils();
+
+            if (File.Exists(StoragePath + "\\AIRange.xml"))
+            {
+                config = utils.ReadFromXmlFile<Config>(StoragePath + "\\AIRange.xml");
+                utils.WriteToXmlFile<Config>(StoragePath + "\\AIRange.xml", config, false);
+            }
+            else
+            {
+                config = new Config();
+                utils.WriteToXmlFile<Config>(StoragePath + "\\AIRange.xml", config, false);
+            }
+
+        }
+
+        private void SessionChanged(ITorchSession session, TorchSessionState newState)
+        {
+
+        }
+        public static Config config;
+    }
+}
+
---
+++ AI Range Extender/Patching.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using SpaceEngineers.Game.EntityComponents.Blocks;
+using Torch.Managers.PatchManager;
+using VRage.Game;
+
+namespace AI_Range_Extender
+{
+    [PatchShim]
+    public class Patching
+    {
+        internal static readonly MethodInfo InitCall =
+            typeof(MySearchEnemyComponent).GetMethod("Init", BindingFlags.Instance | BindingFlags.Public) ??
+            throw new Exception("Failed to find patch method");
+
+        internal static readonly MethodInfo PatchInit =
+            typeof(Patching).GetMethod(nameof(DoThePatch), BindingFlags.Static | BindingFlags.Public) ??
+            throw new Exception("Failed to find patch method");
+
+        public static void Patch(PatchContext ctx)
+        {
+            ctx.GetPattern(InitCall).Prefixes.Add(PatchInit);
+        }
+
+        public static void DoThePatch(ref MySearchEnemyComponent __instance, ref MyComponentDefinitionBase definition)
+        {
+            var def = definition as MySearchEnemyComponentDefinition;
+            def.SearchRadius = Core.config.NewAIBlockRange;
+        }
+    }
+}
+
